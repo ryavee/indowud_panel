9f3e13992aa876c3bf229fe018cd63033da14289
ryavee <y.ryavee9611@gmail.com>
2025-09-19 18:27:07 +0530

    settings page updated

--- src/Pages/Settings.jsx
+++ src/Pages/Settings.jsx
@@ -1,8 +1,8 @@
 const Settings = () => {
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-bold">Catalogue</h1>
-      <p className="text-gray-600 mt-2">This is where you’ll manage product catalogues.</p>
+      <h1 className="text-2xl font-bold">Settings</h1>
+      <p className="text-gray-600 mt-2">This is where you’ll manage Settings.</p>
     </div>
   );
 };
