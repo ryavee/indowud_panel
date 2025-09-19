28cb88504132c1e44db983ecc6244f16c34f4291
ryavee <y.ryavee9611@gmail.com>
2025-09-19 18:29:26 +0530

    settings page updated

--- src/Pages/Settings.jsx
+++ src/Pages/Settings.jsx
@@ -2,7 +2,7 @@ const Settings = () => {
   return (
     <div className="p-6">
       <h1 className="text-2xl font-bold">Settings</h1>
-      <p className="text-gray-600 mt-2">This is where you’ll manage Settings.</p>
+      <p className="text-gray-600 mt-2">This is where We will manage Settings.</p>
     </div>
   );
 };
