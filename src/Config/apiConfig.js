4fd225e4062c3208ef6003b1e37653bd051cf1f6
nasihDev <nasihameen902@gmail.com>
2025-09-22 10:47:21 +0530

    update api url

--- src/Config/apiConfig.js
+++ src/Config/apiConfig.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:2000";
+export const BASE_URL = "https://indowud-main-engine.onrender.com";
 export const ENDPOINTS = {
   LOGIN: "/api/v1/auth/login",
   GETUSERDATA: "/api/v1/auth/users/:uid",
