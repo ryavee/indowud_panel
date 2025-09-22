80c8f485d7e3b00b9251f764ea71fbb4f4d82865
nasihDev <nasihameen902@gmail.com>
2025-09-22 18:02:59 +0530

    Live Url updated

--- src/Config/apiConfig.js
+++ src/Config/apiConfig.js
@@ -1,5 +1,6 @@
-export const BASE_URL = "http://localhost:2000";
-// "https://indowud-main-engine.onrender.com";
+export const BASE_URL = 
+// "http://localhost:2000";
+"https://indowud-main-engine.onrender.com";
 export const ENDPOINTS = {
   LOGIN: "/api/v1/auth/login",
   GETUSERDATA: "/api/v1/auth/users/:uid",
